80498ae10587a747c5244742ae05d76bdd50e2ab
User <User@DESKTOP-JFL46Q2>
2024-09-30 01:42:36 +0300

    Proje dosyası ekle.

---
+++ İlkUygulama/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ilkUygulama
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Merve Çelik");
+            Console.WriteLine("Yazılım dünyasına iyi bir giriş yapmak istediğim için Patika'nın Bootcampine katıldım! ");
+        }
+    }
+
+
+}
+
+
